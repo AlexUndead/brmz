7d0d3f42b6b34ab15b5f6f55048fc9e5fd5e795e
Alex <Alex_Neha_92@mail.ru>
2020-09-11 17:19:12 +0300

    Сделал новые страницы

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('about_us/', views.about_us, name='about_us'),
+    path('contacts/', views.contacts, name='contacts'),
+    path('legitimacy_activities/', views.legitimacy_activities, name='legitimacy_activities'),
+    path('download_catalog/', views.download_catalog, name='download_catalog'),
+    path('news/', views.news, name='news'),
     path('', views.index, name='index'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html', context={})
+
+def about_us(request):
+    return render(request, 'about_us.html', context={})
+
+def contacts(request):
+    return render(request, 'contacts.html', context={})
+
+def legitimacy_activities(request):
+    return render(request, 'legitimacy_activities.html', context={})
+
+def download_catalog(request):
+    return render(request, 'download_catalog.html', context={})
+
+def news(request):
+    return render(request, 'in_development.html', context={})

5bb3010542bb23a8ca9536fb9c8fd48f33fb55d5
Alex <Alex_Neha_92@mail.ru>
2020-09-14 22:03:11 +0300

    Добавление хоста

--- brmz/settings.py
+++ brmz/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False')
 
-ALLOWED_HOSTS = ['brmz.herokuapp.com']
+ALLOWED_HOSTS = ['brmz.herokuapp.com', 'www.br-mz.ru']
 
 
 # Application definition
